6f24550db381a0efffc9002f8480b428cd22ff3c
Kaopong Sangkittiyakorn <84172946+kuisskui@users.noreply.github.com>
2025-01-25 17:14:06 +0700

    no creating new function

--- app/error.tsx
+++ app/error.tsx
@@ -20,7 +20,7 @@ export default function Error({error, reset,}: {
             <h1 className="text-6xl font-bold">Oops!</h1>
             <p className="text-xl mt-4 mb-2">Something Went Wrong</p>
             <p className="mb-8">It might have been removed, renamed, or did not exist in the first place.</p>
-            <button className="mb-2" name={"try again"} onClick={reset}> try again</button>
+            <button className="mb-2" onClick={reset}> try again</button>
             <Button name={"Go to Home page"} command={navigate}/>
         </div>
     )
